62929ce982201555669992d5a35b956aca4020e0
donetkit <donetkit@gmail.com>
2022-04-20 14:56:18 +0800

    timeout

---
+++ _example/timeout/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/donetkit/gin-contrib/timeout"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func emptySuccessResponse(c *gin.Context) {
+	time.Sleep(5 * time.Second)
+	c.String(http.StatusOK, "OK")
+}
+
+func main() {
+	r := gin.New()
+
+	r.GET("/", timeout.New(
+		timeout.WithTimeout(3*time.Second),
+		timeout.WithHandler(emptySuccessResponse),
+	))
+
+	// Listen and Server in 0.0.0.0:8080
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
