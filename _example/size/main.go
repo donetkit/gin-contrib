7e74361cd9db187d30d44801ffdb7dddcc9fda12
donetkit <donetkit@gmail.com>
2022-04-20 16:50:53 +0800

    size

---
+++ _example/size/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	limits "github.com/donetkit/gin-contrib/size"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handler(ctx *gin.Context) {
+	val := ctx.PostForm("b")
+	if len(ctx.Errors) > 0 {
+		return
+	}
+	ctx.String(http.StatusOK, "got %s\n", val)
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(limits.RequestSizeLimiter(10))
+	r.POST("/", handler)
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
