701114555e6cac6fe50b8a56aa634afce8a249cc
donetkit <donetkit@gmail.com>
2022-05-11 13:22:08 +0800

    server

--- _example/server/main.go
+++ _example/server/main.go
@@ -5,17 +5,12 @@ import (
 	logger2 "github.com/donetkit/gin-contrib/middleware/logger"
 	"github.com/donetkit/gin-contrib/server"
 	"github.com/donetkit/gin-contrib/utils/glog"
-	"github.com/donetkit/gin-contrib/utils/uuid"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
 )
 
 func main() {
-	id := uuid.GenerateGoogleUUID()
-	fmt.Println(id)
-	id, _ = uuid.GenerateUUID()
-	fmt.Println(id)
 	logs := glog.NewDefaultLogger()
 	r := gin.New()
 	r.Use(logger2.New(logger2.WithLogger(logs)))
