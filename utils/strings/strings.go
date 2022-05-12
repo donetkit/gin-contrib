d76c754f0c3b3c8cb418529627090791a9e593c0
donetkit <donetkit@gmail.com>
2022-05-12 09:33:15 +0800

    RandomString

---
+++ utils/strings/strings.go
@@ -0,0 +1,18 @@
+package strings
+
+import (
+	"math/rand"
+	"time"
+)
+
+// RandomString fun
+func RandomString(length int) string {
+	str := "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+	b := []byte(str)
+	var result []byte
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, b[r.Intn(len(b))])
+	}
+	return string(result)
+}
