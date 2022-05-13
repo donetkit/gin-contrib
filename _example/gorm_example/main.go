faf5e805a965749dd9a7f4ff9ec3abd84578eca7
donetkit <donetkit@gmail.com>
2022-05-13 15:49:29 +0800

    gorm

--- _example/gorm_example/main.go
+++ _example/gorm_example/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/donetkit/gin-contrib-log/glog"
 	"github.com/donetkit/gin-contrib/db/gorm"
 	"github.com/donetkit/gin-contrib/trace"
 )
 
 const (
-	service     = "app_or_package_name"
+	service     = "gorm-test"
 	environment = "development" // "production" "development"
 )
 
@@ -17,28 +16,39 @@ func main() {
 	ctx := context.Background()
 	log := glog.NewDefaultLogger()
 	var traceServer *trace.Server
-	tp, err := trace.NewTracerProvider(service, "192.168.5.110", environment, 6831)
+	tp, err := trace.NewTracerProvider(service, "127.0.0.1", environment, 6831)
 	if err == nil {
 		jaeger := trace.Jaeger{}
 		traceServer = trace.New(service, trace.WithTracerProvider(tp), trace.WithPropagators(jaeger))
 	}
 
 	var dns = map[string]string{}
-	dns["default"] = "root:zxw123456@tcp(192.168.5.110:3306)/go_red_sentinel?charset=utf8mb4&parseTime=True&loc=Local&timeout=1000ms"
+	dns["default"] = "root:root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local&timeout=1000ms"
 	dbs := gorm.NewDb(gorm.WithDNS(dns), gorm.WithLogger(log), gorm.WithTracer(traceServer))
 
 	defer func() {
 		tp.Shutdown(context.Background())
 	}()
-	ctx, span := traceServer.Tracer.Start(ctx, "calendar")
+	ctx, span := traceServer.Tracer.Start(ctx, "testgorm")
 	defer span.End()
+	var num []int
+	if err := dbs["default"].WithContext(ctx).Raw("SELECT holiday FROM calendar").Scan(&num).Error; err != nil {
+		panic(err)
+	}
 
-	for _, db := range dbs {
-		var num []int
-		if err := db.WithContext(ctx).Raw("SELECT holiday FROM  calendar").Scan(&num).Error; err != nil {
+	var str []string
+	if err := dbs["default"].WithContext(ctx).Raw("SELECT id FROM school").Scan(&str).Error; err != nil {
+		panic(err)
+	}
+
+	if err := dbs["default"].WithContext(ctx).Raw("SELECT id FROM school_air_conditioner_room").Scan(&str).Error; err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 50; i++ {
+		if err := dbs["default"].WithContext(ctx).Raw("SELECT id FROM school_air_conditioner_room").Scan(&str).Error; err != nil {
 			panic(err)
 		}
-		fmt.Println(num)
 	}
 
 }
