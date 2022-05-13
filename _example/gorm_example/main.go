58ff095a9c5dca265706520abbd406fe8a597a1b
donetkit <donetkit@gmail.com>
2022-05-13 17:50:01 +0800

    gorm

--- _example/gorm_example/main.go
+++ _example/gorm_example/main.go
@@ -21,32 +21,30 @@ func main() {
 		jaeger := trace.Jaeger{}
 		traceServer = trace.New(service, trace.WithTracerProvider(tp), trace.WithPropagators(jaeger))
 	}
-
 	var dns = map[string]string{}
-	dns["default"] = "root:root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local&timeout=1000ms"
-	dbs := gorm.NewDb(gorm.WithDNS(dns), gorm.WithLogger(log), gorm.WithTracer(traceServer))
-
+	dns["default"] = "root:test@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local&timeout=1000ms"
+	sql := gorm.NewDb(gorm.WithDNS(dns), gorm.WithLogger(log), gorm.WithTracer(traceServer))
 	defer func() {
 		tp.Shutdown(context.Background())
 	}()
 	ctx, span := traceServer.Tracer.Start(ctx, "testgorm")
 	defer span.End()
 	var num []int
-	if err := dbs["default"].WithContext(ctx).Raw("SELECT holiday FROM calendar").Scan(&num).Error; err != nil {
+	if err := sql.DB().WithContext(ctx).Raw("SELECT holiday FROM calendar where id != ''").Scan(&num).Error; err != nil {
 		panic(err)
 	}
 
 	var str []string
-	if err := dbs["default"].WithContext(ctx).Raw("SELECT id FROM school").Scan(&str).Error; err != nil {
+	if err := sql.DB().WithContext(ctx).Raw("SELECT id FROM school").Scan(&str).Error; err != nil {
 		panic(err)
 	}
 
-	if err := dbs["default"].WithContext(ctx).Raw("SELECT id FROM school_air_conditioner_room").Scan(&str).Error; err != nil {
+	if err := sql.DB().WithContext(ctx).Raw("SELECT id FROM school_air_conditioner_room").Scan(&str).Error; err != nil {
 		panic(err)
 	}
 
 	for i := 0; i < 50; i++ {
-		if err := dbs["default"].WithContext(ctx).Raw("SELECT id FROM school_air_conditioner_room").Scan(&str).Error; err != nil {
+		if err := sql.DB().WithContext(ctx).Raw("SELECT id FROM school_air_conditioner_room").Scan(&str).Error; err != nil {
 			panic(err)
 		}
 	}
