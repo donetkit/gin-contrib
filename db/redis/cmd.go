cb80b660a2b18c11887ed5d4db3cd9f68e8ac889
donetkit <donetkit@gmail.com>
2022-05-15 16:52:22 +0800

    cache

--- db/redis/cmd.go
+++ db/redis/cmd.go
@@ -2,10 +2,13 @@ package redis
 
 import (
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/donetkit/gin-contrib/utils/strings"
 	"github.com/donetkit/gin-contrib/utils/uuid"
 	"github.com/go-redis/redis/v8"
+	"strconv"
 	"time"
 )
 
@@ -38,20 +41,18 @@ func (r *Client) GetString(db int, key string, c ...context.Context) (string, er
 }
 
 //Set 设置一个值
-func (r *Client) Set(db int, key string, val interface{}, timeout time.Duration, c ...context.Context) (err error) {
+func (r *Client) Set(db int, key string, val interface{}, timeout time.Duration, c ...context.Context) error {
+	var err error
 	var ctx = r.config.ctx
 	if len(c) > 0 {
 		ctx = c[0]
 	}
 	var data []byte
 	if data, err = json.Marshal(val); err != nil {
-		return
+		return err
 	}
 	err = r.Client[db].Set(ctx, key, data, timeout).Err()
-	if err != nil {
-		panic(err)
-	}
-	return
+	return err
 }
 
 //IsExist 判断key是否存在
@@ -206,4 +207,142 @@ func (r *Client) ReleaseLock(db int, lockName, code string, c ...context.Context
 			return false
 		}
 	}
+}
+
+func CmdString(cmd redis.Cmder) string {
+	b := make([]byte, 0, 32)
+	b = AppendCmd(b, cmd)
+	return strings.String(b)
+}
+
+func CmdsString(cmds []redis.Cmder) (string, string) {
+	const numCmdLimit = 100
+	const numNameLimit = 10
+
+	seen := make(map[string]struct{}, numNameLimit)
+	unqNames := make([]string, 0, numNameLimit)
+
+	b := make([]byte, 0, 32*len(cmds))
+
+	for i, cmd := range cmds {
+		if i > numCmdLimit {
+			break
+		}
+
+		if i > 0 {
+			b = append(b, '\n')
+		}
+		b = AppendCmd(b, cmd)
+
+		if len(unqNames) >= numNameLimit {
+			continue
+		}
+
+		name := cmd.FullName()
+		if _, ok := seen[name]; !ok {
+			seen[name] = struct{}{}
+			unqNames = append(unqNames, name)
+		}
+	}
+
+	summary := strings.Join(unqNames, " ")
+	return summary, strings.String(b)
+}
+
+func AppendCmd(b []byte, cmd redis.Cmder) []byte {
+	const numArgLimit = 32
+
+	for i, arg := range cmd.Args() {
+		if i > numArgLimit {
+			break
+		}
+		if i > 0 {
+			b = append(b, ' ')
+		}
+		b = appendArg(b, arg)
+	}
+
+	if err := cmd.Err(); err != nil {
+		b = append(b, ": "...)
+		b = append(b, err.Error()...)
+	}
+
+	return b
+}
+
+func appendArg(b []byte, v interface{}) []byte {
+	const argLenLimit = 64
+
+	switch v := v.(type) {
+	case nil:
+		return append(b, "<nil>"...)
+	case string:
+		if len(v) > argLenLimit {
+			v = v[:argLenLimit]
+		}
+		return appendUTF8String(b, strings.Bytes(v))
+	case []byte:
+		if len(v) > argLenLimit {
+			v = v[:argLenLimit]
+		}
+		return appendUTF8String(b, v)
+	case int:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int8:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int16:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int32:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int64:
+		return strconv.AppendInt(b, v, 10)
+	case uint:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint8:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint16:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint32:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint64:
+		return strconv.AppendUint(b, v, 10)
+	case float32:
+		return strconv.AppendFloat(b, float64(v), 'f', -1, 64)
+	case float64:
+		return strconv.AppendFloat(b, v, 'f', -1, 64)
+	case bool:
+		if v {
+			return append(b, "true"...)
+		}
+		return append(b, "false"...)
+	case time.Time:
+		return v.AppendFormat(b, time.RFC3339Nano)
+	default:
+		return append(b, fmt.Sprint(v)...)
+	}
+}
+
+func appendUTF8String(dst []byte, src []byte) []byte {
+	if isSimple(src) {
+		dst = append(dst, src...)
+		return dst
+	}
+
+	s := len(dst)
+	dst = append(dst, make([]byte, hex.EncodedLen(len(src)))...)
+	hex.Encode(dst[s:], src)
+	return dst
+}
+
+func isSimple(b []byte) bool {
+	for _, c := range b {
+		if !isSimpleByte(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func isSimpleByte(c byte) bool {
+	return c >= 0x21 && c <= 0x7e
 }
---
+++ utils/cache/cache.go
@@ -0,0 +1,22 @@
+package cache
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+type Cache interface {
+	Get(db int, key string, c ...context.Context) interface{}
+	GetString(db int, key string, c ...context.Context) (string, error)
+	Set(db int, key string, val interface{}, timeout time.Duration, c ...context.Context) error
+	IsExist(db int, key string, c ...context.Context) bool
+	Delete(db int, key string, c ...context.Context) (int64, error)
+	LPush(db int, key string, values interface{}, c ...context.Context) (int64, error)
+	RPop(db int, key string, c ...context.Context) interface{}
+	XRead(db int, key string, count int64, c ...context.Context) ([]redis.XStream, error)
+	XAdd(db int, key, id string, values []string, c ...context.Context) (string, error)
+	XDel(db int, key string, id string, c ...context.Context) (int64, error)
+	GetLock(db int, lockName string, acquireTimeout, lockTimeOut time.Duration, c ...context.Context) (string, error)
+	ReleaseLock(db int, lockName, code string, c ...context.Context) bool
+}
