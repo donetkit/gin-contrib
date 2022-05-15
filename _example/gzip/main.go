a2a5edb5466b71568528c9a11d9c4e35789d6520
donetkit <donetkit@gmail.com>
2022-05-15 16:27:24 +0800

    log

--- _example/gzip/main.go
+++ _example/gzip/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/donetkit/gin-contrib-log/glog"
 	"github.com/donetkit/gin-contrib/middleware/gzip"
-	"log"
+	"github.com/donetkit/gin-contrib/middleware/logger"
 	"net/http"
 	"time"
 
@@ -11,12 +12,25 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.Use(gzip.Gzip(gzip.DefaultCompression))
+	log := glog.New()
+	logger.SetGinDefaultWriter(log)
+	r := gin.New()
+	// LoggerWithFormatter 中间件会将日志写入 gin.DefaultWriter
+	// By default gin.DefaultWriter = os.Stdout
+
+	//gin.DefaultWriter =
+	r.Use(gzip.Gzip(gzip.DefaultCompression), logger.New(logger.WithLogger(log)))
+
+	//
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
-
+	r.GET("/ping2", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
+	})
+	r.GET("/ping3", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
+	})
 	// Listen and Server in 0.0.0.0:8080
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
